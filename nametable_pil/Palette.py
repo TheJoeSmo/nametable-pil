d7514c38aad98d3054fdfd0805daa1a1be028305
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-10-20 21:19:26 -0400

    Fix Palette typing issues

--- nametable_pil/Palette.py
+++ nametable_pil/Palette.py
@@ -1,4 +1,3 @@
-from functools import cached_property
 from typing import Protocol
 
 from attr import attrib, attrs
@@ -11,13 +10,15 @@ from nametable_pil.validators import rgb_validator
 class PaletteProtocol(Protocol):
     palette: ImagePalette
 
-    colors: tuple[Color, ...]
+    @property
+    def colors(self) -> tuple[Color, ...]:
+        ...
 
 
 @attrs(frozen=True)
 class Palette:
     palette: ImagePalette = attrib(validator=rgb_validator)
 
-    @cached_property
+    @property
     def colors(self) -> tuple[Color, ...]:
         return tuple(generate_from_bytes(self.palette.tobytes(), self.palette.mode))
